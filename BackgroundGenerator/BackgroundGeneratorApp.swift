6d9c2ff0c26233d0fef5890b812f692a637b3e3f
Alex Jordany <alexjordany@outlook.com>
2025-08-28 17:37:07 -0600

    Initial Commit

---
+++ BackgroundGenerator/BackgroundGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundGeneratorApp.swift
+//  BackgroundGenerator
+//
+//  Created by Alex Parrales on 28/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
